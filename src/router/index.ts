f3225802ba3e86bc1b4afae84c18431ace80235a
theoria <theoria23@gmail.com>
2021-10-21 00:48:17 +0900

    色々マシに
    - リストのレンダリングをVueっぽく
    - 見た話数を1話ずつ設定可に
    - 画像生成をマシに

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    component: () => import('/@/pages/Index.vue'),
+    component: () => import('../pages/Index.vue'),
   },
 ];
 
